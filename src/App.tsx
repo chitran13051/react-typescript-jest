afaf123c4db0191cbcb72ee4526941168bb0a05f
Chi Tran <khanhchi.tran13051@gmail.com>
2021-08-25 13:44:31 +0200

    added prettier typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
 // import helloWorld from "Components/test";
 
-import helloWorld from "Components/test";
+import helloWorld from "Components/test"
 helloWorld()
-function App(x : any) {
+function App(x: any) {
   return (
     <div className="App">
-    <h1> Hello Webstorm </h1>
-             <header>
+      <h1> Hello Webstorm </h1>
+      <header>
         Change Commit
-        <h1>Wanna Change Commit</h1>
+        <h1>Wanna Change Commit</h1>{" "}
       </header>
-      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
